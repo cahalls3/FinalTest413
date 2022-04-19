8fbdd8995ffc748f6176d7ec25dbfa65448f79bd
cahalls3 <carsonahalls3@gmail.com>
2022-04-18 20:01:30 -0600

    Creation

---
+++ FinalTest413/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FinalTest413.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
